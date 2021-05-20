09f10584e94f1b30cec3ea5b84a6428dfc36321d
daisybutler <daisybutler16@gmail.com>
2021-05-20 10:16:50 +0000

    Landing page text and image.

--- workbench/settings.py
+++ workbench/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', #Required to load media files
             ],
         },
     },
@@ -149,6 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
