22587596ea2e343f11f25cdd52b1869e9a944d96
daisybutler <daisybutler16@gmail.com>
2021-05-24 09:41:37 +0000

    Created Plan model.

---
+++ plans/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    features = models.TextField()
+
+    def __str__(self):
+        return self.name
