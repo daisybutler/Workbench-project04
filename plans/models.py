d85d578059ec96d55f7c694bb1b44408d45ef70e
daisybutler <daisybutler16@gmail.com>
2021-05-24 10:45:47 +0000

    Added friendly name field to plans model.

--- plans/models.py
+++ plans/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Plan(models.Model):
     name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     features = models.TextField()
 
