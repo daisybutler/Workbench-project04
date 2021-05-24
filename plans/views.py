75173b67091faba040e1f1dc0bcfcac54764de14
daisybutler <daisybutler16@gmail.com>
2021-05-24 11:11:17 +0000

    Created all_plans view and urls.

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Plan
 
-# Create your views here.
+
+def all_plans(request):
+    """A view to display all plans."""
+
+    plans = Plan.objects.all()
+
+    context = {
+        'plans': plans,
+    }
+
+    return render(request, 'plans/plans.html', context)
