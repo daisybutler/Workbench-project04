cc81889238b6ad46723b34f121806a9ba7e7d3ea
daisybutler <daisybutler16@gmail.com>
2021-05-25 09:31:03 +0000

    Added view and template for individual plan page.

--- plans/urls.py
+++ plans/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_plans, name='all_plans')
+    path('', views.all_plans, name='all_plans'),
+    path('<name>', views.individual_plan, name='individual_plan'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Plan
 
 
@@ -12,3 +12,15 @@ def all_plans(request):
     }
 
     return render(request, 'plans/plans.html', context)
+
+
+def individual_plan(request, name):
+    """A view to display details of an individual plan."""
+
+    plan = Plan.objects.get(name=name)
+
+    context = {
+        'plan': plan,
+    }
+
+    return render(request, 'plans/individual-plan.html', context)
