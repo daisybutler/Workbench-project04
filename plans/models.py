9d215e8f0e9d723a7ce51103a399ca2638a11321
daisybutler <daisybutler16@gmail.com>
2021-05-25 07:52:36 +0000

    Added additional fields to plans model.

--- plans/models.py
+++ plans/models.py
@@ -5,7 +5,9 @@ class Plan(models.Model):
     name = models.CharField(max_length=200)
     friendly_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    features = models.TextField()
+    plan_type = models.CharField(max_length=25)
+    short_description = models.TextField()
+    long_description = models.TextField()
 
     def __str__(self):
         return self.name
