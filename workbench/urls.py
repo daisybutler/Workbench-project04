02e08f336635bbcab853e87b97f1a8daa803feb8
daisybutler <daisybutler16@gmail.com>
2021-06-07 12:18:44 +0000

    Added checkout url

--- workbench/urls.py
+++ workbench/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('plans', include('plans.urls')),
     path('locations', include('locations.urls')),
+    path('checkout', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
