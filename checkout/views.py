22ed80c4bc3276eb3db6e86aeceaca12ba5d6b5a
daisybutler <daisybutler16@gmail.com>
2021-06-07 10:18:24 +0000

    Basic layout for order summary template.

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
+
 def checkout_order(request):
     """A view that renders the order summary page"""
-    
-    return render(request, 'checkout/checkout-order.html')
 
+    return render(request, 'checkout/checkout-order.html')
