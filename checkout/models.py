68347cadfab77aa10e9e5be6e7f695bc85b9c74d
daisybutler <daisybutler16@gmail.com>
2021-06-09 09:25:39 +0000

    Created order model.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=32, null=False, editable=False)
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200)
+    qty = models.IntegerField(null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    plan_type = models.CharField(max_length=25)
+    short_description = models.TextField()
+    long_description = models.TextField()
+
+    def __str__(self):
+        return self.order_id + self.name
