8f8bc215d160ddcd686d0554cc8028b536816c25
daisybutler <daisybutler16@gmail.com>
2021-06-11 09:19:59 +0000

    Fixed model migration issues by recreating db.sqlite database.

--- checkout/models.py
+++ checkout/models.py
@@ -1,15 +1,41 @@
+import uuid
 from django.db import models
+from django.conf import settings
 
 
 class Order(models.Model):
     order_id = models.CharField(max_length=32, null=False, editable=False)
-    name = models.CharField(max_length=200)
-    friendly_name = models.CharField(max_length=200)
+    plan_name = models.CharField(max_length=200)
+    plan_type = models.CharField(max_length=25)
+    plan_friendly_name = models.CharField(max_length=200)
     qty = models.IntegerField(null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    plan_type = models.CharField(max_length=25)
-    short_description = models.TextField()
-    long_description = models.TextField()
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_id(self):
+        """
+        Generate a random, unique order ID using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order ID
+        if it hasn't been set already.
+        """
+        if not self.order_id:
+            self.order_id = self._generate_order_id()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.order_id + self.name
+        return self.order_id
