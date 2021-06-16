3c533b5e02219573028589a7a6dd5fcd2fcfd283
daisybutler <daisybutler16@gmail.com>
2021-06-16 11:56:29 +0000

    Added client test key and pk.

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from plans.models import Plan
-import datetime
+from .models import Order
 
 
 def checkout_order(request, name):
@@ -10,6 +10,9 @@ def checkout_order(request, name):
 
     context = {
         'order': order,
+        'stripe_public_key': 'pk_test_51J2w92Gvf3CD91ZSzCMQT2SmD2pJVGAOJoYcImcZbhKny0SbHqH8jVmghzMQObtdd8nRKzqju6LCc0UjN1aFimT700YPnEvt3Q',
+        'client_secret': 'test client secret',
+
     }
 
     return render(request, 'checkout/checkout-order.html', context=context)
