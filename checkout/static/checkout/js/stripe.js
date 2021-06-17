aca48d92151ac38931c9fcdf4d13aa8f05ac060c
daisybutler <daisybutler16@gmail.com>
2021-06-17 11:36:58 +0000

    Display and validate Stripe card details input.

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,5 +1,37 @@
-
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#313131',
+        fontFamily: '"Open Sans", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#313131',
+        }
+    },
+    invalid: {
+        color: '#D91F44',
+        iconColor: '#D91F44'
+    }
+};
+var card = elements.create('card', { style: style });
+card.mount('#card-details');
+
+// Handle validation errors of card details
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContext = '';
+    }
+});
