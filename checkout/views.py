71aedea6c04b2cf5496327742a9790d25d2016a1
daisybutler <daisybutler16@gmail.com>
2021-06-17 11:39:58 +0000

    Test stripe with pk and false client secret.

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,6 @@ def checkout_order(request, name):
         'order': order,
         'stripe_public_key': 'pk_test_51J2w92Gvf3CD91ZSzCMQT2SmD2pJVGAOJoYcImcZbhKny0SbHqH8jVmghzMQObtdd8nRKzqju6LCc0UjN1aFimT700YPnEvt3Q',
         'client_secret': 'test client secret',
-
     }
 
     return render(request, 'checkout/checkout-order.html', context=context)
