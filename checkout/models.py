1b324ff43c4a410e5476bdbd36e414cc93e0e63a
daisybutler <daisybutler16@gmail.com>
2021-06-25 13:10:54 +0000

    Added defaults to int/decimal fields in Order model.

--- checkout/models.py
+++ checkout/models.py
@@ -5,15 +5,17 @@ from django.conf import settings
 
 class Order(models.Model):
     order_id = models.CharField(max_length=32, null=False, editable=False)
-    plan_name = models.CharField(max_length=200)
-    plan_type = models.CharField(max_length=25)
-    plan_friendly_name = models.CharField(max_length=200)
-    qty = models.IntegerField(null=False, blank=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
+    plan_name = models.CharField(max_length=200, null=False, blank=False)
+    plan_type = models.CharField(max_length=25, null=False, blank=False)
+    plan_friendly_name = models.CharField(
+        max_length=200, null=False, blank=False)
+    qty = models.IntegerField(null=False, blank=False, default=0)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    last_name = models.CharField(max_length=25, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    password = models.CharField(max_length=25)
+    password = models.CharField(max_length=25, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     billing_address = models.CharField(max_length=80, null=False, blank=False)
