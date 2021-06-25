952984db3b54478cecfd8d7535e0a6a5c55a5d1b
daisybutler <daisybutler16@gmail.com>
2021-06-25 11:51:50 +0000

    Created OrderAdmin class for admin display or orders.

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Order
 
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('order_id', 'date',
+                       'qty', 'price')
+
+    fields = ('order_id', 'date', 'plan_name', 'plan_type',
+              'plan_friendly_name', 'qty', 'price', 'first_name',
+              'last_name', 'email', 'password', 'phone_number',
+              'postcode', 'billing_address', 'county')
+
+    list_display = ('order_id', 'date', 'first_name',
+                    'last_name', 'plan_name', 'qty',
+                    'price')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
