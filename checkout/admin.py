0b36fd9ddfc5de1644c89a6467f853c8d698710e
daisybutler <daisybutler16@gmail.com>
2021-07-02 11:15:05 +0000

    Updated location field.

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,17 +4,16 @@ from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
 
-    readonly_fields = ('order_id', 'date',
-                       'qty', 'price')
+    readonly_fields = ('order_id', 'date', 'price')
 
     fields = ('order_id', 'date', 'plan_name', 'plan_type',
-              'plan_friendly_name', 'qty', 'price', 'first_name',
+              'plan_friendly_name', 'location', 'price', 'first_name',
               'last_name', 'email', 'password', 'phone_number',
-              'postcode', 'billing_address', 'county')
+              'postcode', 'billing_address', 'county', 'stripe_pid')
 
     list_display = ('order_id', 'date', 'first_name',
-                    'last_name', 'plan_name', 'qty',
-                    'price')
+                    'last_name', 'plan_name', 'location',
+                    'price', 'stripe_pid')
 
     ordering = ('-date',)
 
