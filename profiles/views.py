37dda21522afffa772d81ad7e3deee951c130356
daisybutler <daisybutler16@gmail.com>
2021-07-05 13:39:40 +0000

    Removed login url and added template and context to profile url.

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-
-
-def login(request):
-    """A view to display login page."""
-
-    return render(request, 'profiles/login.html')
+from .models import UserProfile
 
 
 def profile(request):
     """Display user's profile"""
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
