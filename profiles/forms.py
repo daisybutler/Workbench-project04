7bd34802eadd44dc83030165433359a0b5ef729f
daisybutler <daisybutler16@gmail.com>
2021-07-06 09:55:07 +0000

    Created user profile form and rendered in template.

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,26 @@
 from django import forms
+from .models import UserProfile
 
 
-class UserLogin(forms.Form):
-    email = forms.CharField(label='email', max_length=40),
-    password = forms.CharField(label='password', max_length=40),
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and set autofocus of
+        first field.
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_first_name': 'Full Name',
+            'default_last_name': 'Last Name',
+            'default_email': 'Email Address',
+            'default_phone_number': 'Phone Number',
+            'default_billing_address': 'Billing Address',
+            'default_postcode': 'Postcode',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -7,8 +8,11 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+
     template = 'profiles/profile.html'
     context = {
+        'form': form,
         'profile': profile,
     }
 
