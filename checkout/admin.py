b06826fa7aa96724a61712020a959f55e45eb2c5
daisybutler <daisybutler16@gmail.com>
2021-07-06 13:37:55 +0000

    Saving user details for later.

--- checkout/admin.py
+++ checkout/admin.py
@@ -6,7 +6,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_id', 'date', 'price')
 
-    fields = ('order_id', 'date', 'plan_name', 'plan_type',
+    fields = ('order_id', 'user_profile', 'date', 'plan_name', 'plan_type',
               'plan_friendly_name', 'location', 'price', 'first_name',
               'last_name', 'email', 'password', 'phone_number',
               'postcode', 'billing_address', 'county', 'stripe_pid')
