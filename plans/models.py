5c19b9fc79ac0224b92619c9a0ecf15181f5e210
daisybutler <daisybutler16@gmail.com>
2021-07-08 19:25:51 +0000

    Added price_id field for subscriptions.

--- plans/models.py
+++ plans/models.py
@@ -5,6 +5,7 @@ class Plan(models.Model):
     name = models.CharField(max_length=200)
     friendly_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    price_id = models.CharField(max_length=200)
     plan_type = models.CharField(max_length=25)
     short_description = models.TextField()
     long_description = models.TextField()
