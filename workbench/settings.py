ac6001196326e9700ea29414cdec38dc2f7a0c15
daisybutler <daisybutler16@gmail.com>
2021-07-28 13:24:48 +0000

    Check if in dev mode for emails to console or not.

--- workbench/settings.py
+++ workbench/settings.py
@@ -109,13 +109,19 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 # Email Notifications
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('GMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
-EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = 'team@workbench.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+if 'DEVELOPMENT' in os.environ:
+    DEFAULT_FROM_EMAIL = 'team@workbench.com'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+else:
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('GMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+    EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = 'team@workbench.com'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Account Requirements
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
